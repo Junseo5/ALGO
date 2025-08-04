0f59c2196645729b9ff21334b0b1758371413f6c
Junseo5 <junseo042626@gmail.com>
2025-08-04 16:14:16 +0900

    ADD D2_16504

---
+++ D2/D2_16504.py
@@ -0,0 +1,18 @@
+# 16504. Gravity
+
+tc = int(input())
+
+for T in range(tc):
+    n = int(input())
+    boxs = list(map(int, input().split()))
+
+    result = boxs[0]
+    cnt = 0
+
+    for i in range(len(boxs)):
+        if result > boxs[i]:
+            cnt += 1
+        elif cnt < 1:
+            result = boxs[i]
+        
+    print(f"#{T+1} {cnt}")
