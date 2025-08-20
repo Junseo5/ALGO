4d698b90f073a9dbfdf3884b60371ab49cab2dde
Junseo5 <junseo042626@gmail.com>
2025-08-20 15:28:35 +0900

    2805

---
+++ Solving_18_Club/Combination/Combination_2805.py
@@ -0,0 +1,16 @@
+# 2805 . 농작물 수확하기
+
+tc = int(input())
+
+for T in range(tc):
+    n = int(input())
+    n_list = [list(map(int, input())) for _ in range(n)]
+
+    total = 0
+
+    for i in range(n):
+        for j in range(n):
+            if abs(i - (n // 2)) + abs(j - (n // 2)) <= (n // 2):
+                total += n_list[i][j]
+
+    print(f"#{T + 1} {total}")
