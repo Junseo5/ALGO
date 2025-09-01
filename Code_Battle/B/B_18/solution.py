6fe77c651aabc96e8335708ea04ab573824632ca
Junseo5 <junseo042626@gmail.com>
2025-09-01 22:13:22 +0900

    Save: B_18 try 3

--- Code_Battle/B/B_18/solution.py
+++ Code_Battle/B/B_18/solution.py
@@ -36,42 +36,59 @@ def top5Keyword(mRet):
     # 개수 같은 경우 문자값 비교 후 더 작은 값을 대표로 추가
     
 
-    dq_set_list = set(dq_list)
-    save_list = []
-
-    # for i in dq_set_list:
-    #     for j in dq_set_list:
-    #         # 문자열 길이가 같지 않을 경우 유사어 아님
-    #         if len(i) != len(j):
-    #             continue
-    #         cnt = 0
-    #         # 길이가 같은 문자열 두개를 한단어씩 비교
-    #         for ch1, ch2 in zip(i, j):
-    #             if ch1 != ch2:
-    #                 cnt += 1
-    #                 if cnt > 1:
-    #                     break
-    
-
+    dq_set_list = list(set(dq_list))
+    count_dict = Counter(dq_list)
     same_len_dict = defaultdict(list)
-    same_list = []
 
-    for i in dq_set_list:
-        same_len_dict[len(i)].append(i)
+    for word in dq_set_list:
+        same_len_dict[len(word)].append(word)
 
-    for i in same_len_dict.values():
+    index_dict = {w: idx for idx, w in enumerate(dq_set_list)}
+    graph = [[] for _ in range(len(dq_set_list))]
+
+    for words in same_len_dict.values():
         same_dict = defaultdict(list)
+        for w in words:
+            for pos in range(len(w)):
+                temp = w[:pos] + '*' + w[pos+1:]
+                same_dict[temp].append(w)
+        for mask, same_words in same_dict.items():
+            if len(same_words) > 1:
+                for i in range(len(same_words)):
+                    for j in range(i+1, len(same_words)):
+                        a, b = same_words[i], same_words[j]
+                        graph[index_dict[a]].append(index_dict[b])
+                        graph[index_dict[b]].append(index_dict[a])
 
-        for j in i:
-            for k in range(len(j)):
-                temp = j[:k] + '@' + j[k+1:]
-                same_dict[temp].append(j)
-        
-        for origin, same in same_dict.items():
-            if len(same) > 1:
-                for j in range(len(same)):
-                    for k in range(j+1, len(same)):
-                        same_list.append((same[j], same[k]))
-    
+    # 2. BFS로 그룹 찾기
+    visited = [False] * len(dq_set_list)
+    groups_info = []  # (대표어, 그룹총전달수)
+
+    for i in range(len(dq_set_list)):
+        if not visited[i]:
+            q = deque([i])
+            visited[i] = True
+            group_words = []
+            while q:
+                cur = q.popleft()
+                group_words.append(dq_set_list[cur])
+                for nxt in graph[cur]:
+                    if not visited[nxt]:
+                        visited[nxt] = True
+                        q.append(nxt)
+            # 대표어 선정 (전체 기준 count_dict 사용)
+            max_freq = max(count_dict[w] for w in group_words)
+            candidates = [w for w in group_words if count_dict[w] == max_freq]
+            rep_word = min(candidates)  # 동률이면 사전순
+            # 그룹 총 전달 횟수 (전체 기준)
+            group_total = sum(count_dict[w] for w in group_words)
+            groups_info.append((rep_word, group_total))
+
+    # 3. 인기 검색어 정렬 (전달수 내림차순 → 대표어 사전순)
+    groups_info.sort(key=lambda x: (-x[1], x[0]))
+
+    # 4. 상위 5개 대표어 출력
+    top_5 = [rep for rep, _ in groups_info[:5]]
+    mRet = top_5
 
     return len(mRet)
